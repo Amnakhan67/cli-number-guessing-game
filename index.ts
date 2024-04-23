11638f9fc6fce75a796473aa1c518c848fb2a800
AmnaKhan67 <ahmed6789opiu@gmail.com>
2024-04-23 10:20:53 +0500

    Adding cli number guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+console.log(`\n\tWelcome to Amna's cli number guessing game\n\t`);
+
+ import inquirer from "inquirer";
+ let randomNumber=Math.floor(Math.random()*4+1);
+ let answer=await inquirer.prompt([
+    {name:"userguessedNumber",
+        type:"number",
+        message:"guess a number(number limit is 1 to 5)",
+    }
+ ]);
+ if(answer.userguessedNumber===randomNumber){
+    console.log(`congratulations, you guessed the correct number`);
+ }
+ else {
+    console.log(`Sorry! you guessed the wrong number`)};
